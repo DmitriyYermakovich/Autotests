26f497bbc61a8dc9274f0be93e6a95c85e16e194
DmitriyYermakovich <d.yermakovich@sdventures.com>
2019-06-03 18:07:06 +0300

    Открытие "http://www.nbc.com и закрытие окна браузера.

---
+++ Project_Yermakovich_1/Project_Yermakovich_1/Tests/Open_Site_NBC.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Project_Yermakovich_1.Tests
+{
+    public class HBC_Test
+    {
+        [Test]
+        public void NBC()
+        {
+            Assert.That(true, Is.True);
+        }
+        [Test]
+        public void SeleniumTest()
+        {
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://www.nbc.com");
+            driver.Manage().Window.Maximize();
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(5));
+            driver.Quit();
+        }
+
+    }
+
+}
