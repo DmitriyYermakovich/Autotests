4fae4116a9fd074b2d297107ce922230cacb47c1
DmitriyYermakovich <d.yermakovich@sdventures.com>
2019-06-03 14:01:31 +0300

    Project first.

---
+++ Project_Yermakovich_1/Project_Yermakovich_1/Class1.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Text;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Sample_Test_1()
+        {
+            Assert.That(true, Is.True);
+        }
+    }
+}
